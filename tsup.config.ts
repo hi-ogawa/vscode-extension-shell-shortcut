7ae1b309dc2d8549a0f54b4f1861c9074be99560
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-30 15:44:46 +0900

    chore: cleanup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from "tsup";
 
-// export default [
-//   defineConfig({
-//     entry: {
-//       "client/worker-entry": "src/client/worker-entry.ts",
-//       "client/vite-node": "src/client/vite-node.ts",
-//     },
-//     platform: "node",
-//     external: ["vscode"]
-//   }),
-// ]
-
 export default defineConfig({
   entry: ["src/extension.ts"],
   platform: "node",
