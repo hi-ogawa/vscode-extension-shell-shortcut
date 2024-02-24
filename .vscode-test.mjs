5911ea6ad75d6a53926a7c56f30c6adb8783558e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-24 11:36:46 +0900

    test: use vscode-test cli

---
+++ .vscode-test.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "@vscode/test-cli";
+
+export default defineConfig({
+  files: "src/test/suite/**/*.test.ts",
+  mocha: {
+    preload: "./src/test/preload.cjs",
+  }
+});
