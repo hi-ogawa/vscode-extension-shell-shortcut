93da06f4409540a31bce9b0cc5ad9ab6f4e2f895
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-28 14:43:03 +0900

    test: simplify tsx setup

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "@vscode/test-cli";
 export default defineConfig({
   files: "src/test/suite/**/*.test.ts",
   mocha: {
-    preload: "./src/test/preload.cjs",
+    preload: "tsx/cjs",
   },
 });
