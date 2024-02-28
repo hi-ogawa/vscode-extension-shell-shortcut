3e1c5094ed5d45d71641f27cde3ff02f1cc391c2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-28 19:31:53 +0900

    test: setup extest + vitest

--- src/test-e2e/basic.test.ts
+++ src/test-e2e/basic.test.ts
@@ -1,3 +1,18 @@
 import { test } from "vitest";
+import {
+  VSBrowser,
+  ReleaseQuality,
+  VSCODE_VERSION_MAX,
+  ExTester,
+} from "vscode-extension-tester";
 
-test("basic", () => {});
+test("basic", async () => {
+  const browser = new VSBrowser(
+    VSCODE_VERSION_MAX,
+    ReleaseQuality.Stable,
+  );
+  const extester = new ExTester();
+  await browser.start((extester as any).code.executablePath);
+  await browser.waitForWorkbench();
+  await browser.quit();
+});
