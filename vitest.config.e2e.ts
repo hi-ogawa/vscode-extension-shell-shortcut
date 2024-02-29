de25b9ef9f7411a53943407e1a713d5c5a062296
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-29 13:17:56 +0900

    chore: config

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     dir: "./src/test-e2e",
     maxWorkers: 1,
     fileParallelism: false,
-    testTimeout: process.env.CI ? undefined : Infinity,
+    testTimeout: process.env.CI ? 60_000 : Infinity,
     env: {
       CODE_VERSION: "1.86.2",
       TEST_RESOURCES: "node_modules/.cache/extest",
