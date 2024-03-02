bada34741987dabd677e4adc981da6610cc3d245
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-02 16:52:11 +0900

    test: save trace

--- src/test-e2e/utils-test.ts
+++ src/test-e2e/utils-test.ts
@@ -1,5 +1,6 @@
 import { launchVscode } from "./utils";
 import { test } from "vitest";
+import npath from "node:path";
 
 type VscodeTestFixture = {
   __internal: Awaited<ReturnType<typeof launchVscode>>;
@@ -16,10 +17,24 @@ export const vscodeTest = test.extend<VscodeTestFixture>({
     await use(result);
     await result.app.close();
   },
-  app: async ({ __internal }, use) => {
-    await use(__internal.app);
+  app: async ({ __internal: { app } }, use) => {
+    await use(app);
   },
-  page: async ({ __internal }, use) => {
-    await use(__internal.page);
+  page: async ({ __internal: { page }, task }, use) => {
+    // https://github.com/microsoft/playwright-vscode/blob/1c2f766a3ef4b7633fb19103a3d930ebe385250e/tests-integration/tests/baseTest.ts#L59
+    await page
+      .context()
+      .tracing.start({ title: task.name, screenshots: true, snapshots: true });
+    await use(page);
+    await page.context().tracing.stop({
+      path: npath.resolve(
+        `test-results`,
+        [npath.basename(task.file?.filepath!), task.name, task.id]
+          .filter(Boolean)
+          .join("-")
+          .replaceAll(/\W/g, "-"),
+        "trace.zip",
+      ),
+    });
   },
 });
